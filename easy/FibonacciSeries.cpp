6c68a99cccbbbafc4ab1ab0f94e40e14019293b9
Yohei Marion Okuyama <marionwkya@gmail.com>
2014-04-15 11:54:40 +0900

    soved Fibonacci Series with recursive call

--- easy/FibonacciSeries.cpp
+++ easy/FibonacciSeries.cpp
@@ -5,19 +5,9 @@ using namespace std;
 
 int run(int n)
 {
-	if (n == 0) return 0;
+	if (n <= 0) return 0;
 	if (n == 1) return 1;
-	//cout << n << " : ";
-	int a = 0;
-	int b = 1;
-	int c;
-	for (int i = 2; i <= n; i++){
-		c = a + b;
-		a = b;
-		b = c;
-	}
-	return c;
-
+	return run(n - 1) + run(n - 2);
 }
 
 int main(int argc, const char* argv[])
