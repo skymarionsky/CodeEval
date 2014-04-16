25a1d369ac21d486adb474b77d91e93ac168b354
Yohei Marion Okuyama <marionwkya@gmail.com>
2014-04-16 21:18:28 +0900

    solved Odd Numbers

---
+++ easy/OddNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+	for (int i = 1; i <= 99; i += 2){
+		cout << i << endl;
+	}
+
+	return 0;
+}
